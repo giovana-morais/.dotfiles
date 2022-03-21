f05971a574c4f9ebcef31061bfb47dc45a02b762
Giovana Morais <giovana.vmorais@gmail.com>
2022-03-21 17:55:27 -0300

    reorganize everything

--- install.sh
+++ install.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
 
-sudo apt install nvim zsh tmux python3-pip ruby-full build-essential zlib1g-dev \
-ffmpeg -y
+sudo apt update
+
+function install {
+	which $1 &> /dev/null
+
+	if [ $? -ne 0 ]; then
+		echo "installing ${1}"
+		sudo apt install -y $1
+	else
+		echo "$1 already installed"
+	fi
+}
+
+install build-essential
+install cowsay
+install ffmpeg
+install latexmk
+install nvim
+install python3-pip
+install ruby-full
+install tmux
+install zathura
+install zsh
+
+# reduce overheating
+sudo add-apt-repository ppa:linrunner/tlp
+sudo apt-get update
+sudo apt-get install tlp tlp-rdw
 
 chsh -s /bin/zsh
 
@@ -12,36 +38,41 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
+# create simlinks
+ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
+ln -sf ~/.dotfiles/jupyter ~/.jupyter
+ln -sf ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -sf ~/.dotfiles/vim ~/.vim
+ln -sf ~/.dotfiles/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/zshrc ~/.zshrc
+ln -sf ~/.dotfiles/nvim_init ~/.config/init.vim
+
 ## vim
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # install plugins from .vimrc using command line
-vim -es -u vimrc -i NONE -c "PlugInstall" -c "qa"
-
-# create simlinks
-ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-ln -s ~/.dotfiles/jupyter ~/.jupyter
-ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/vim ~/.vim
-ln -s ~/.dotfiles/vimrc ~/.vimrc
-ln -s ~/.dotfiles/zshrc ~/.zshrc
-ln -s ~/.dotfiles/nvim_init ~/.config/init.vim
+vim +PlugInstall +qall
 
 # python, virtualenv and lots of libs
-pip install virtualenv
 cd ~
+pip install virtualenv
 virtualenv venv
 source venv/bin/activate
-pip install numpy matplotlib scipy librosa essentia
+pip install numpy
+pip install matplotlib
+pip install scipy
+pip install librosa
+pip install essentia
 deactivate
 
-
 # ruby, jekyll and etc
-echo '# Install Ruby Gems to ~/gems' >> ~/.zshrc
+echo '# install Ruby Gems to ~/gems' >> ~/.zshrc
 echo 'export GEM_HOME="$HOME/gems"' >> ~/.zshrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.zshrc
 source ~/.zshrc
 
 sudo gem install jekyll bundler
+
+cowsay -s i guess we are done!
